161b35b1b04e61305b92b10879ff68155cb3c624
mugugoo <dafifth@hotmail.fr>
2020-11-10 01:02:51 +0100

    nothing much

--- js.js
+++ js.js
@@ -1,9 +1,19 @@
-let nodeButton=document.querySelectorAll('button');
-nodeButton.forEach( elem => {
-if (elem.textContent*1===NaN)
-elem.classList.add('other');
-else elem.classList.add('number');
-}
-)
-nodeButton.forEach( (button)=> {
+let previousResult=0;
+let currentNumber=0;
+
+let divDisplay=document.getElementById('display')
+console.log(divDisplay.value);
+let buttonCalcul=document.querySelectorAll('button.input-calc');
+//console.log(buttonCalcul)
+buttonCalcul.forEach( el => {
+
+el.addEventListener('click', () => displayNum(divDisplay, event));
 });
+
+
+function displayNum(divD, event) {
+console.log(divD.value);
+if (divD.value==="0") divD.value="5"
+else divD.textContent+=5;
+return;
+}

6497a606e9df7812ee7625c92248f9ee7b21730d
mugugoo <dafifth@hotmail.fr>
2020-11-11 17:15:15 +0100

    Was thinking of creating a web page with multiples Calculator, that you can spawn with a button This is the last front end challenge, so I figured I would end it big I'm thinking about adding functionnalities like interactivity between calculator

--- js.js
+++ js.js
@@ -3,15 +3,18 @@
 //Creating a calculator object, to avoid using global variables
 //don't know how to use contructors yet
 function createCalculatorNode(
-  Node=document.querySelector('div.container.calculator')) {
-let calcNode=getElementById('calculator-display-1')
-let calcDisplayWin=document.createElement("textarea ")
-let calcResultWin=document.createElement("textarea #result")
-Node.append(calcDisplayWin,calcResultWin)
+  Node=document.querySelector('div #calculator-display')) {
+let calcNode=document.getElementById('calculator-display-1')
+let calcDisplayWin=document.createElement("textarea")
+let calcResultWin=document.createElement("textarea")
+//to give a particular ID? use getElementID in a for loop to check if taken
+//if taken, idname-[number] i++ //if null, idname
+console.log(Node)
+return Node.append(calcResultWin, calcDisplayWin)
 }
 
 createCalculatorNode();
-calculator.display="0", memory="0";
+let calculator={display:"0", memory:"0"}
 //creating the calculator methods
 calculator.reset = () => {
     displayNode.textcontent="0";
@@ -21,10 +24,9 @@ calculator.add = n => {
     display= this.memory;
 }
 
-let div=document.querySelector('div.container.calculator-display')
+let div=document.querySelector('div.container #calculator-display')
 div.prepend(calculator);
-let 
-calculator.display4RL = function() {}
+calculator.displayRL = function() {}
 
 let woo
 let buttonCalcul=document.querySelectorAll('button.input-calc');
